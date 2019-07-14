f5cee232eb785597cd777527b8b4b9a44088e03d
J Q <edwardapp@outlook.com>
2019-07-14 23:48:17 +0800

    v21

--- htv2.py
+++ htv2.py
@@ -6,9 +6,10 @@ import urllib
 import codecs
 import queue
 import threading
-import time
-
+import os
+import re
 exitflag = False
+volexitflag = False
 reqheaders = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'}
 
@@ -45,16 +46,15 @@ class get_commic(object):
             self.book.append({'name': each.a.get('title'),
                               'href': each.a.get('href')})
 
-    # 获取每卷中的每页url，并存入json  
+    # 获取每卷中的每页url，并存入json
     def get_pagelist(self):
         global exitflag
         bookqueue = queue.Queue()
-        volqueue = queue.Queue()
         vols = []
         thread_pages = []
         # 开启4线程
         for i in range(4):
-            t = thread_page(bookqueue, volqueue, vols, self.base)
+            t = thread_page(bookqueue, vols, self.base)
             t.start()
             thread_pages.append(t)
 
@@ -73,14 +73,47 @@ class get_commic(object):
             print('end thread')
         # print(repr(vols))
 
-        # DUMP TO JSON     utf-8-sig
+        # DUMP TO JSON     utf-8
         with codecs.open(self.bookname+'_thread.json', 'w', encoding="utf-8") as f:
             json.dump(vols, f, ensure_ascii=False)
+
+    def get_piclist(self):
+        with open(self.bookname+'_thread.json', 'rb') as f:
+            vols = json.load(f)
+        global volexitflag
+        picqueue = queue.Queue()
+        thread_pics = []
+        pics = []
+        # 开启10线程
+        for i in range(10):
+            t = thread_pic(picqueue, self.base, pics)
+            t.start()
+            thread_pics.append(t)
+
+        # 填充queue
+        for vol in vols:
+            for pic in vol['pages']:
+                picqueue.put([vol['name'], pic])
+
+        # 等待队列处理完成
+        while not picqueue.empty():
+            pass
+        volexitflag = True
+
+        # 退出线程
+        for thread in thread_pics:
+            thread.join()
+
+        # DUMP TO JSON     utf-8
+        with codecs.open(self.bookname+'_pic_thread.json', 'w', encoding="utf-8") as f:
+            json.dump(pics, f, ensure_ascii=False)
+
+        print('done')
 
 
 # 抓取page线程
 class thread_page(threading.Thread):
-    def __init__(self, bookqueue, volqueue, vols, baseurl):
+    def __init__(self, bookqueue, vols, baseurl):
         threading.Thread.__init__(self)
         self.bookqueue = bookqueue
         self.vols = vols
@@ -120,8 +153,49 @@ class thread_page(threading.Thread):
         # print('done')
 
 
+class thread_pic(threading.Thread):
+    def __init__(self, picqueue, baseurl, pics):
+        threading.Thread.__init__(self)
+        self.picqueue = picqueue
+        self.baseurl = baseurl
+        self.pics = pics
+
+    def run(self):
+        while not volexitflag:
+            try:
+                pic = self.picqueue.get()
+                # print(pic[0], pic[1])
+                # 每卷一个目录
+                if not os.path.exists(os.path.join(os.path.abspath('.'), pic[0])):
+                    os.mkdir(os.path.join(os.path.abspath('.'), pic[0]))
+
+                # 获取页码
+                pagex = re.match(r'.*p(\d*).*', pic[1])
+                if len(pagex.group(1)) == 0:
+                    x = '1'
+                else:
+                    x = pagex.group(1)
+                # print(x)
+
+                # 获取pic URL
+                html = requests.get(
+                    url=self.baseurl + pic[1], headers=reqheaders, verify=False).text
+                picurl = BeautifulSoup(html, features="lxml").find(
+                    'img', class_='img-fluid').get('src')
+                print(x, picurl)
+
+                self.pics.append([pic[0], x, self.baseurl + picurl])
+
+                # # DOWNLOAD, test
+                # with open(os.path.join(os.path.abspath('.'), pic[0], x + '.json'), 'w', encoding="utf-8") as f:
+                #     f.write(pic[0]+x + self.baseurl + picurl)
+
+            except:
+                pass
+
+
 if __name__ == '__main__':
     commic = get_commic()
-    commic.get_booklist()
-    commic.get_pagelist()
-    # commic.get_piclist()
+    # commic.get_booklist()
+    # commic.get_pagelist()
+    commic.get_piclist()
--- temp.py
+++ temp.py
@@ -3,32 +3,37 @@ import json
 import sys
 import urllib
 from bs4 import BeautifulSoup
+import os
+os.path.abspath('')
+newfolder = os.path.join(os.path.abspath(''),'new folder')
+# os.mkdir(newfolder)
+print('xxxx','abc')
+with open (newfolder+'\\x.txt', 'w' ) as f:
+    f.write('xxxx','abc')
 
-class b(object):
-    def __init__(self):
-        self.name='b'
-    def runb(self):
-        d=[]
-        c=a(d)
-        c.runa()
+# class b(object):
+#     def __init__(self):
+#         self.name='b'
+#     def runb(self):
+#         d=[]
+#         c=a(d)
+#         c.runa()
 
-        print(repr(d))
+#         print(repr(d))
 
-class a(object):
-    def __init__(self,d):
-        self.bookname = 'HunterHunter'
-        self.d = d
-    def runa(self):
-        for i in range(5):
-            self.d.append(i)
-        print(self.bookname)
-
-
+# class a(object):
+#     def __init__(self,d):
+#         self.bookname = 'HunterHunter'
+#         self.d = d
+#     def runa(self):
+#         for i in range(5):
+#             self.d.append(i)
+#         print(self.bookname)
 
 
 
 
-mainb = b()
-mainb.runb()
+# mainb = b()
+# mainb.runb()
 
         
