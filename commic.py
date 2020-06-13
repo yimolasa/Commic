6cdf7d7fe029d4fdb2ca7c7a7f80aede372ca371
J Q <edwardapp@outlook.com>
2020-06-14 00:21:13 +0800

    add comment

--- commic.py
+++ commic.py
@@ -47,7 +47,7 @@ def get_vol():
     for each in li:
         book.append({'name': each.a.get('title'),
                      'href': base + each.a.get('href')})
-
+    # dump json {"name": VOLname, "href": URL}
     with codecs.open(booklist, 'w', encoding="utf-8") as f:
         json.dump(book, f, ensure_ascii=False)
 
@@ -68,6 +68,7 @@ def access_vol(lastinfo, vols):
     else:  # fresh start
         todownloadvols = vols
         startpage = 1
+    # start to download
     for vol in todownloadvols:
         volfolder = os.path.join(bookfolder, vol['name'])
         volurl = vol['href']
@@ -82,9 +83,11 @@ def get_page(volurl, volfolder, startpage):
     browser.get(volurl)
     WebDriverWait(browser, 3)
 
+    # total page in VOL
     pages = browser.find_element_by_xpath(
         '//*[@id="page-selector"]/option[last()]').get_attribute('value')
     
+    # jump to start page
     if startpage > 1:
         browser.find_element_by_xpath('//*[@id="page-selector"]/option['+str(startpage)+']').click()
 
@@ -93,23 +96,23 @@ def get_page(volurl, volfolder, startpage):
         pagename = os.path.join(volfolder, str(i)+'.jpg')
         wait.until(EC.presence_of_element_located((By.ID, "all")))
         #  with a try catch later & a file existing detect
-        if not os.path.exists(pagename):
-            imgurl = browser.find_element_by_xpath(
-                '//*[@id="all"]/div/div[2]/img').get_attribute('src')
+        # if not os.path.exists(pagename):
+        imgurl = browser.find_element_by_xpath(
+            '//*[@id="all"]/div/div[2]/img').get_attribute('src')
+        try:
+            urllib.request.urlretrieve(imgurl, pagename) # downloading
+        except:
+            time.sleep(3)
             try:
-                urllib.request.urlretrieve(imgurl, pagename)
+                urllib.request.urlretrieve(imgurl, pagename) # one more try if failed
             except:
-                time.sleep(3)
-                try:
-                    urllib.request.urlretrieve(imgurl, pagename)
-                except:
-                    msg = os.path.basename(
-                        volfolder) + ',' + str(i) + ',' + pages + ',' + imgurl
-                    dlog(msg, errorlog)
-            else:
-                msg = os.path.basename(volfolder) + ',' + \
-                    str(i) + ',' + pages + ',' + imgurl
-                dlog(msg, downloadlog)
+                msg = os.path.basename(
+                    volfolder) + ',' + str(i) + ',' + pages + ',' + imgurl
+                dlog(msg, errorlog)
+        else:
+            msg = os.path.basename(volfolder) + ',' + \
+                str(i) + ',' + pages + ',' + imgurl
+            dlog(msg, downloadlog)
         print(str(i))
 
         if i == int(pages):
@@ -121,14 +124,14 @@ def get_page(volurl, volfolder, startpage):
 
 
 def lastbreak(downloadlog):
-    if os.stat(downloadlog).st_size == 0:
+    if os.stat(downloadlog).st_size == 0: # if new log, fresh download
         return(0)
     else:
         with open(downloadlog, 'r', encoding="utf-8") as f:
             for lastline in f:
                 pass
             lastinfo = lastline.split(',')
-            if lastinfo[1] == lastinfo[2]:
+            if lastinfo[1] == lastinfo[2]: # if last page of the vol
                 lastend = 1
             else:
                 lastend = 0
@@ -158,12 +161,6 @@ def main():
         vols = json.load(f)
     access_vol(lastinfo, vols)
 
-    # for vol in vols:
-    #     volfolder = os.path.join(bookfolder, vol['name'])
-    #     volurl = vol['href']
-    #     if not os.path.exists(volfolder):
-    #         os.mkdir(volfolder)
-    #     get_page(volurl, volfolder)
     browser.close()
 
 
