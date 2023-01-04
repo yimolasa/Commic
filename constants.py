aca310d2d21fe83b7ea00f75d490e3ee3e882085
yimolasa <edwardapp@outlook.com>
2023-01-04 21:10:24 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,5 +7,5 @@ StartpageXpath = '//*[@id="page-selector"]/option['
 ImgXpath = '//*[@id="all"]/div/div[2]/img'
 # NextpageXpath = '/html/body/div/div[1]/nav/div/a[3]'
 NextpageXpath = '//*[@id="right"]'
-bookname = 'Alice'
-homepage = 'https://www.manhuadb.com/manhua/1247'  # Hunter * Hunter
+bookname = 'Jojo4'
+homepage = 'https://www.manhuadb.com/manhua/124'  # Hunter * Hunter
